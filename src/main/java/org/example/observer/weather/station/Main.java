ce3d0fe1892d79f298bebae7e6e285e5b1970fae
satyanand deva <62107720+satyanand-droid@users.noreply.github.com>
2024-10-28 19:42:35 +0530

    update

--- src/main/java/org/example/observer/weather/station/Main.java
+++ src/main/java/org/example/observer/weather/station/Main.java
@@ -2,11 +2,10 @@ package org.example.observer.weather.station;
 
 public class Main {
     public static void main(String[] args){
-            Observer currentCondition = new CurrentConditions();
-            Observer weatherForecast = new WeatherForecast();
+
             WeatherData weatherData = new WeatherData();
-            weatherData.register(currentCondition);
-            weatherData.register(weatherForecast);
+            Observer currentCondition = new CurrentConditions(weatherData);
+            Observer weatherForecast = new WeatherForecast(weatherData);
             weatherData.setMeasurements(10,20,30);
             weatherData.measurementChanged();
     }
--- src/main/java/org/example/observer/weather/station/WeatherForecast.java
+++ src/main/java/org/example/observer/weather/station/WeatherForecast.java
@@ -1,6 +1,12 @@
 package org.example.observer.weather.station;
 
 public class WeatherForecast implements Observer {
+    private State WeatherData;
+
+    public WeatherForecast(State weatherData){
+        this.WeatherData=weatherData;
+        weatherData.register(this);
+    }
     @Override
     public void update(int temperature, int pressure, int humidity) {
     System.out.println("inside weather forecast ");
